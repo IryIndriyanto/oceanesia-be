57828661d683214a76610871ff3123ccb238f267
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-04-30 07:10:30 -0700

    create issue table and user table

--- models/issue.py
+++ models/issue.py
@@ -1,4 +1,19 @@
 from db import db
+from enum import Enum
+
+
+class IssueCategory(Enum):
+    TRASH = "trash"
+    POLLUTION = "pollution"
+    EROSION = "erosion"
+    UNSAFE = "unsafe"
+    OTHER = "other"
+
+
+class IssueStatus(Enum):
+    OPEN = "withdrawal"
+    IN_PROGRESS = "in_progress"
+    CLOSED = "closed"
 
 
 class IssueModel(db.Model):
@@ -9,8 +24,8 @@ class IssueModel(db.Model):
     location_id = db.Column(db.Integer, db.ForeignKey("location.id"))
     issue_title = db.Column(db.String(255))
     issue_image = db.Column(db.String(255))
-    issue_category = db.Column(db.Enum("Category1", "Category2", "Category3"))
-    issue_status = db.Column(db.Enum("Open", "In Progress", "Closed"))
+    issue_category = db.Column(db.Enum(IssueCategory))
+    issue_status = db.Column(db.Enum(IssueStatus))
     issue_description = db.Column(db.Text)
     created_at = db.Column(
         db.DateTime(timezone=True), default=db.func.now(), nullable=False
