36dc77f5d7f5dfe7273bb47f0abfcaaac72317f2
Iry Indriyanto <iry.indriyanto20@gmail.com>
2024-05-03 03:00:51 -0700

    fix wrong schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,12 +1,6 @@
 from marshmallow import Schema, fields
 
 class UserSchema(Schema):
-    id = fields.UUID(dump_only=True)
     username = fields.Str(required=True)
-    email = fields.Str()
-    password_hash = fields.Str(required=True)
-
-class UserUpdateSchema(Schema):
-    username = fields.Str()
-    password_hash = fields.Str()
-    email = fields.Str()
+    email = fields.Email(required=True)
+    password = fields.Str(required=True)
