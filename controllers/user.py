a20e93a6b2559d7367a25bfaa3ab46fb51cf7815
CaturAgeng <caturageng023@gmail.com>
2024-05-07 00:57:42 +0700

    Add google-oauth in user login and README.md for Oceanesia BE

--- controllers/user.py
+++ controllers/user.py
@@ -1,3 +1,4 @@
+import os
 from db import db
 from flask import jsonify
 from flask_smorest import Blueprint
@@ -12,8 +13,22 @@ from flask_jwt_extended import (
     get_jwt_identity,
 )
 
+from authlib.integrations.flask_client import OAuth
+
 blp = Blueprint("users", "users", description="data of users", url_prefix="/users")
 
+oauth = OAuth()
+
+oauth.register(
+    name='google',
+    client_id= os.getenv('GOOGLE_CLIENT_ID'),
+    client_secret= os.getenv('GOOGLE_CLIENT_SECRET'),
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    # authorize_params=None,
+    authorize_params={'scope': 'email profile'},
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    client_kwargs={'scope': 'openid profile email'},
+)
 
 @blp.route("/", methods=["POST"])
 @blp.arguments(UserSchema)
@@ -35,15 +50,36 @@ def user_register(user_data):
 @blp.arguments(UserSchema)
 @blp.response(200)
 def user_login(user_data):
-    user = UserModel.query.filter_by(username=user_data["username"]).first()
-    if user and pbkdf2_sha256.verify(user_data["password"], user.password_hash):
-        access_token = create_access_token(identity=user.id, fresh=True)
-        refresh_token = create_refresh_token(identity=user.id)
-        return (
-            jsonify({"access token": access_token, "refresh token": refresh_token}),
-            200,
-        )
-    return jsonify({"message": "Invalid credentials"}), 401
+    if 'google_token' in user_data:
+        # Jika ada token OAuth 2.0 Google, lakukan autentikasi dengan Google
+        token = user_data['google_token']
+        resp = oauth.google.get('userinfo', token=token)
+        if resp.status_code == 200:
+            userinfo = resp.json()
+            user = UserModel.query.filter_by(email=userinfo['email']).first()
+            if not user:
+                user = UserModel(
+                    username=userinfo['email'].split('@')[0],
+                    email=userinfo['email'],
+                    password_hash='',  # hash password dikosongkan karena OAuth
+                )
+                db.session.add(user)
+                db.session.commit()
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+            return jsonify({"access_token": access_token, "refresh_token": refresh_token}), 200
+        return jsonify({"message": "Invalid OAuth token"}), 401
+    else: 
+        # Jika tidak ada token OAuth 2.0 Google, lakukan autentikasi biasa dengan username dan password
+        user = UserModel.query.filter_by(username=user_data["username"]).first()
+        if user and pbkdf2_sha256.verify(user_data["password"], user.password_hash):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(identity=user.id)
+            return (
+                jsonify({"access token": access_token, "refresh token": refresh_token}),
+                200,
+            )
+        return jsonify({"message": "Invalid credentials"}), 401
 
 
 @blp.route("/me", methods=["GET"])
